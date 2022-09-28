1c4dbdc99148541e1cd59981af7b9dcd7595bf16
HDomi <hwangjae1139@gmail.com>
2022-09-28 10:10:41 +0900

    첫 커밋

---
+++ src/views/MyselfView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="myself">
+    <h1>This is an myself page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .myself {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
